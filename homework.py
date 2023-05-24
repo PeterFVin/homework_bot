b0203206946974b071d816e43770861607ea41a7
PeterFVin <peterwinogradov@gmail.com>
2023-05-24 16:10:10 +0300

    telebot, rev.2

--- homework.py
+++ homework.py
@@ -9,6 +9,21 @@ from dotenv import load_dotenv
 from telegram import Bot
 from telegram.ext import Updater
 
+from exceptions import NoAPIAnswer
+
+"""
+Александр, привет! Не смог тебя найти в пачке по karpova1eks,
+так что пишу здесь уточнения.
+1. В функции check_response ты говоришь сперва проверить наличие homeworks,
+но если я делаю конструкцию типа
+if not response["homeworks"]:
+    -> if not isinstance(response.get("homeworks") и.т.п.
+то такую конструкцию не пропускает pytest. Или ты имел в виду что-то другое?
+2. В main та же ситуация, конструкцию if response["homeworks"] != [] и
+if len(response["homeworks"]) != 0 пропускает pytest,
+а if not response["homeworks"] - не пропускает.
+"""
+
 load_dotenv()
 
 PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
@@ -24,13 +39,6 @@ HOMEWORK_VERDICTS = {
     "reviewing": "Работа взята на проверку ревьюером.",
     "rejected": "Работа проверена: у ревьюера есть замечания.",
 }
-
-
-class NoAPIAnswer(Exception):
-    """Возникает в случае неправильного ответа от сервера."""
-
-    pass
-
 
 updater = Updater(token=TELEGRAM_TOKEN)
 
@@ -52,7 +60,7 @@ def check_tokens():
         "TELEGRAM_CHAT_ID": TELEGRAM_CHAT_ID,
     }
     for varname, varvalue in environment_variables.items():
-        if varvalue is None:
+        if not varvalue:
             logging.critical(f"Отсутствует переменная окружения {varname}!")
             sys.exit()
 
@@ -63,9 +71,8 @@ def send_message(bot, message):
     Функция отправляет сообщение в Telegram-чат об изменении
     статуса домашней работы, а также об ошибках в работе бота.
     """
-    chat_id = TELEGRAM_CHAT_ID
     try:
-        bot.send_message(chat_id, message)
+        bot.send_message(TELEGRAM_CHAT_ID, message)
         logging.debug("Сообщение успешно отправлено в чат!")
     except Exception as error:
         logging.error(f"{error} - сообщение не отправлено в чат!")
@@ -74,24 +81,26 @@ def send_message(bot, message):
 def get_api_answer(timestamp):
     """Функция делает запрос к API и приводит ответ к типу данных Python."""
     try:
-        url = ENDPOINT
-        headers = HEADERS
         payload = {"from_date": timestamp}
         homework_statuses = requests.get(
-            url,
-            headers=headers,
+            ENDPOINT,
+            headers=HEADERS,
             params=payload,
             timeout=10,
         )
         if homework_statuses.status_code != HTTPStatus.OK:
-            raise NoAPIAnswer(bot, message="Ошибка! Неверный ответ сервера!")
+            raise NoAPIAnswer(message="Ошибка! Неверный ответ сервера!")
         return homework_statuses.json()
+
+    except NoAPIAnswer as error:
+        message = "Ошибка! Неверный ответ сервера!"
+        logging.error(message)
+        raise NoAPIAnswer(message, error)
 
     except Exception as error:
         message = "Ошибка! url-адрес недоступен!"
         logging.error(message)
-        send_message(bot, message)
-        raise NoAPIAnswer(message, error)
+        raise Exception(message, error)
 
 
 def check_response(response):
@@ -108,39 +117,27 @@ def check_response(response):
 
 def parse_status(homework):
     """Функция выделяет из ответа API необходимые аргументы."""
-    if "homework_name" not in homework:
-        message = "В домашке отсутствует ключ homework_name"
-        logging.error(message)
-        send_message(bot, message)
-        raise KeyError(message)
+    homework_keys = ['homework_name', 'status']
+    for key in homework_keys:
+        if key not in homework:
+            message = f'В домашке отсутствует ключ {key}'
+            logging.error(message)
+            raise KeyError(message)
 
-    elif "status" not in homework:
-        message = "В домашке отсутствует ключ status"
-        logging.error(message)
-        send_message(bot, message)
-        raise KeyError(message)
-
-    elif homework["status"] not in HOMEWORK_VERDICTS.keys():
+    if homework['status'] not in HOMEWORK_VERDICTS:
         message = "Неожиданное значение под ключём status"
         logging.error(message)
-        send_message(bot, message)
         raise KeyError(message)
 
-    try:
-        verdict = homework["status"]
-        homework_name = homework["homework_name"]
-        return (f'Изменился статус проверки работы "{homework_name}".'
-                f'{HOMEWORK_VERDICTS[verdict]}')
-    except Exception as error:
-        logging.error("Возникла ошибка!", error)
-        send_message(bot, message)
-        raise Exception("Возникла ошибка!", error)
+    verdict = homework["status"]
+    homework_name = homework["homework_name"]
+    return (f'Изменился статус проверки работы "{homework_name}".'
+            f'{HOMEWORK_VERDICTS[verdict]}')
 
 
 def main():
     """Основная логика работы бота."""
     check_tokens()
-    global bot
     bot = Bot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
     send_message(bot, message="Начали парсинг!")
@@ -148,16 +145,16 @@ def main():
         try:
             response = get_api_answer(timestamp)
             check_response(response)
-            if response["homeworks"] != []:
-                homework = response["homeworks"][0]
-                message = parse_status(homework)
+            status_change = response["homeworks"]
+            if status_change != []:
+                first_element, *other_elements = status_change
+                message = parse_status(first_element)
                 send_message(bot, message)
             else:
                 logging.debug("В домашке нет новых статусов!")
         except Exception as error:
             logging.error(f"Сбой в работе программы: {error}")
             send_message(bot, message)
-            raise Exception
         finally:
             time.sleep(RETRY_PERIOD)
 
